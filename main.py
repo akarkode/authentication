a4ea2bebba4ebc214707c40e9155c127d3eb634f
akarkode <79255351+akarkode@users.noreply.github.com>
2025-09-10 08:29:17 +0000

    feat(security): add HTTPS redirect middleware

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 from app.src.router.v1.api import router
 from app.src.core.config import settings
 
 app = FastAPI(title="Authentication Service")
 app.include_router(router=router)
+app.add_middleware(HTTPSRedirectMiddleware)
 app.add_middleware(SessionMiddleware, secret_key=settings.SESSION_SECRET_KEY)
 
 templates = Jinja2Templates(directory="app/src/templates")
