394d8e93fc7b32bf2933edd1a953fca987fe2a8a
akarkode <79255351+akarkode@users.noreply.github.com>
2025-09-17 01:57:19 +0000

    🔧 refactor(auth-service): add API router prefix `/api/auth`

--- app/src/router/v1/api.py
+++ app/src/router/v1/api.py
@@ -4,6 +4,6 @@ from fastapi import APIRouter
 from app.src.router.v1.user.api import router as user
 from app.src.router.v1.google.api import router as google
 
-router = APIRouter(prefix="/v1")
+router = APIRouter(prefix="/api/auth/v1")
 router.include_router(router=user)
 router.include_router(router=google)
